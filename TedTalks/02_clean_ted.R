ed4c5c59b646eaf23c0315cb4c40cc6e1e6f34d0
jesskaminsky <jessica_kaminsky@brown.edu>
2017-11-06 12:09:22 -0500

    monday 11/06

---
+++ TedTalks/02_clean_ted.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+
+ted_main <- read_csv("TedTalks/data/ted_main.csv")
+save(ted_main, file="TedTalks/data/ted_main.Rda")
+
+
+
+ted_clean <- ted_main %>%
+  
+  
+  
+  
+  
+  
+save(ted_clean)
