16d3b7fd110b77b0b26a9ce6b9201ae088e2f285
jesskaminsky <jessica_kaminsky@brown.edu>
2017-11-06 12:27:03 -0500

    monday 11/06 2

--- TedTalks/02_clean_ted.R
+++ TedTalks/02_clean_ted.R
@@ -1,15 +1,21 @@
-library(tidyverse)
-
+##read in main data set
 ted_main <- read_csv("TedTalks/data/ted_main.csv")
-save(ted_main, file="TedTalks/data/ted_main.Rda")
 
+##read in transcripts
+transcripts <- read_csv("TedTalks/data/transcripts.csv")
 
+###combine data sets
+full_data <- inner_join(ted_main, transcripts, by = "url")
 
-ted_clean <- ted_main %>%
-  
-  
-  
-  
-  
-  
-save(ted_clean)
+###remove any text in the transcript that is surrounded by parenthesis
+
+for (i in 1:nrow(full_data)){
+  full_data[i, "transcript"] = 
+    str_replace_all(
+      full_data[i, "transcript"],
+      pattern = "\\([^()]+\\)", 
+      " "
+    )
+}
+
+save(full_data, file = "TedTalks/data/full_data.Rda")
